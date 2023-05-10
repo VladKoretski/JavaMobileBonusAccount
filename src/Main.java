3779325645160bebe44f33ae32f2a8a2e7952e89
Vladko1973 <vpk1973@yandex.ru>
2023-05-10 13:38:56 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
-//Импорт класса Scanner
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+
 //Начальное состояние счета
         float accountBalance = 120f;
         System.out.println("The customer has " + accountBalance + " roubles in they account");
+
 //Объявление переменных (суммы пополнения и минимальной суммы для начисления бонусов) и ввод их значений с клавиатуры
-        Scanner inData = new Scanner(System.in);
-        System.out.println("Input the minimum deposit required for bonus accrual ");
-        float minimumDeposite = inData.nextFloat();
-        System.out.println("Input the deposit amount ");
-        float accountFunding = inData.nextFloat();
-        System.out.println("Input the bonus amount for a hundred roubles ");
-        int bonusForHundredRoubles = inData.nextInt();
+        int minimumDeposite = 500;
+        System.out.println("The minimum deposit required for bonus accrual = " + minimumDeposite + " roubles");
+        float accountFunding = 355;
+        int bonusForHundredRoubles = 1;
+        System.out.println("The amount of bonus for a hundred roubles = " + bonusForHundredRoubles + " rouble(s)");
+        System.out.println("The amount of deposit = " + accountFunding + " roubles");
+
 //Объявление переменных для определения статуса счета (после начисления депозита) и количества бонусов
         int bonusAmount = 0;
         float accountStatus;
 
-        if (accountFunding>=minimumDeposite) {
+        if (accountFunding >= minimumDeposite) {
             bonusAmount = (int) accountFunding / 100;
         }
         accountStatus = (float) accountBalance + bonusAmount + accountFunding;
